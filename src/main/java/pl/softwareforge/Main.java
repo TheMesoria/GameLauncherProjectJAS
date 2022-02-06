406c4adffb0ddd386504517ffcae9463bff02ecb
ormaniec <wojciech.ormaniec@gmail.com>
2022-02-06 20:41:55 +0100

    Initial commit

---
+++ src/main/java/pl/softwareforge/Main.java
@@ -0,0 +1,27 @@
+package pl.softwareforge;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main
+        extends Application
+{
+    @Override
+    public void start( Stage primaryStage ) throws Exception
+    {
+        var fxmlLoader = new FXMLLoader( getClass().getResource( "/view/MainPane.fxml" ) );
+
+        Parent root = fxmlLoader.load();
+        var scene = new Scene( root );
+        primaryStage.setScene( scene );
+        primaryStage.show();
+    }
+
+    public static void main( String[] args )
+    {
+        launch( args );
+    }
+}
---
+++ src/main/java/pl/softwareforge/view/MainPaneView.java
@@ -0,0 +1,13 @@
+package pl.softwareforge.view;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.util.LinkedList;
+
+public class MainPaneView
+{
+
+}
