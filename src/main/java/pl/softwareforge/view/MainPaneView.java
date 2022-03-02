7dbd90e42652037c6ce014294133824bdee3766b
CarlosOlexinho <swobodziaq666@gmail.com>
2022-03-02 20:00:41 +0100

    Update ProfileView fxml and css

--- src/main/java/pl/softwareforge/view/MainPaneView.java
+++ src/main/java/pl/softwareforge/view/MainPaneView.java
@@ -8,72 +8,74 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
 
-public class MainPaneView
-        implements Initializable {
-
-
-    @FXML
-    private AnchorPane MainAnchorPane;
-    @FXML
-    private JFXButton ProfileButton;
-    @FXML
-    private JFXButton SettingsButton;
-
-    int previous = 0;
-    boolean viewLoaded = false;
-
-    @Override
-    public void initialize(URL location,
-                           ResourceBundle resources) {
-        MainAnchorPane.getChildren().clear();
-
-        System.out.println("No issues in initialize!");
-    }
-
-    @FXML
-    void OnMouseClicked(MouseEvent event) {
-        if (event.getSource() == ProfileButton && !viewLoaded) {
-            loadView();
-        } else if (event.getSource() == SettingsButton) {
-            previous++;
-            loadView();
-        } else {
-            viewLoaded = false;
-            MainAnchorPane.getChildren().clear();
-        }
-
-    }
-
-    private void loadView() {
-        try {
-            var fxmlLoader = new FXMLLoader(getClass().getResource("/view/ProfileView.fxml"));
-            Parent root = fxmlLoader.load();
-
-            AnchorPane.setBottomAnchor(root, 0.0);
-            AnchorPane.setTopAnchor(root, 0.0);
-            AnchorPane.setLeftAnchor(root, 0.0);
-            AnchorPane.setRightAnchor(root, 0.0);
-
-            root.getStylesheets().add("/css/SimpleStyling.css");
-            if (previous % 3 == 0) {
-                root.getStylesheets().add("");
-            } else if (previous % 3 == 1) {
-                root.getStylesheets().add("");
-            } else if (previous % 3 == 2) {
-                root.getStylesheets().add("");
-            }
-
-            MainAnchorPane.getChildren().clear();
-            MainAnchorPane.getChildren().add(root);
-
-            viewLoaded = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+//public class MainPaneView
+//        implements Initializable {
+//
+//
+//    @FXML
+//    private BorderPane MainBorderPane;
+//    @FXML
+//    private JFXButton profileButton;
+//    @FXML
+//    private JFXButton storeButton;
+//
+//    int previous = 0;
+//    boolean viewLoaded = false;
+//
+//    @Override
+//    public void initialize(URL location,
+//                           ResourceBundle resources) {
+//        MainBorderPane.getChildren().clear();
+//
+//        System.out.println("No issues in initialize!");
+//    }
+//
+//    @FXML
+//    void OnMouseClicked(MouseEvent event) {
+//        if (event.getSource() == profileButton && !viewLoaded) {
+//            loadView();
+//        } else if (event.getSource() == storeButton) {
+//            previous++;
+//            loadView();
+//        }
+//        else {
+//            viewLoaded = false;
+//            MainBorderPane.getChildren().clear();
+//        }
+//
+//    }
+//
+//    private void loadView() {
+//        try {
+//            var fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainPane.fxml"));
+//            Parent root = fxmlLoader.load();
+//
+//            AnchorPane.setBottomAnchor(root, 0.0);
+//            AnchorPane.setTopAnchor(root, 0.0);
+//            AnchorPane.setLeftAnchor(root, 0.0);
+//            AnchorPane.setRightAnchor(root, 0.0);
+//
+//            root.getStylesheets().add("/css/SimpleStyling.css");
+//            if (previous % 3 == 0) {
+//                root.getStylesheets().add("");
+//            } else if (previous % 3 == 1) {
+//                root.getStylesheets().add("");
+//            } else if (previous % 3 == 2) {
+//                root.getStylesheets().add("");
+//            }
+//
+//            MainBorderPane.getChildren().clear();
+//            MainBorderPane.getChildren().add(root);
+//
+//            viewLoaded = true;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
