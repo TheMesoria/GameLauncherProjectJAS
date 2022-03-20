dc587720c70f306415d947a517c4827b680c68b0
n1ree <dmdanielak@gmail.com>
2022-03-20 10:50:50 +0100

    Delete JFoenix related stuff

--- src/main/java/pl/softwareforge/view/MainPaneView.java
+++ src/main/java/pl/softwareforge/view/MainPaneView.java
@@ -1,7 +1,6 @@
 package pl.softwareforge.view;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextArea;
+
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
